c75261b96f2d3fb515403bfda41765d9e3e38fae
Fernando Dantas <fernando.d.g.leite@gmail.com>
2024-06-10 10:11:12 -0300

    att no favoritos

--- codigo/assents/js/favoritos.js
+++ codigo/assents/js/favoritos.js
@@ -11,20 +11,27 @@ function ListaFavoritos() {
       let card = document.createElement('div');
       card.className = 'col-sm-4 mb-4';
       card.innerHTML = `
-        <div class="card">
+      <div class="card">
           <div class="card-body">
-            <h5 class="card-title">${contato.titulo}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${contato.linguagem}</h6>
-            <p class="card-text">${contato.comando}</p>
-            <p class="card-text"><strong>Dificuldade:</strong> ${contato.dificuldade}</p>
-            <p class="card-text"><strong>Exemplo:</strong> ${contato.exemplo}</p>
-            <button onclick="removeFromFavorites(${contato.id})" class="btn btn-danger mt-2"><i class="fas fa-trash"></i> Remover</button>
+              <h5 class="card-title">${contato.titulo}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${contato.linguagem}</h6>
+              <p class="card-text">${contato.comando}</p>
+              <p class="card-text"><strong>Dificuldade:</strong> ${contato.dificuldade}</p>
+              <p class="card-text"><strong>Exemplo:</strong> ${contato.exemplo}</p>
+              <a href="../pages/desafios.html">
+                  <button class="btn btn-danger mt-2"> Buscar </button>
+              </a>
           </div>
-        </div>
-      `;
+      </div>
+  `;
+  
       favoritosContatos.appendChild(card);
     });
   }
+
+  function redirectToPage() {
+    window.location.href = '../pages/desafios.html';
+}
   
   // Chamada inicial para listar os favoritos ao carregar a página
   ListaFavoritos();
